e80e24b44dd5620ddac0e004f91971718f0e66aa
Michael Twomey <mick@twomeylee.name>
2023-05-08 11:17:22 +0100

    Remove autogenerated comments

--- Formula/blackbox_exporter.rb
+++ Formula/blackbox_exporter.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BlackboxExporter < Formula
   desc "Prometheus exporter for machine metrics"
   homepage "https://prometheus.io/"
